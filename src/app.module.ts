d3161a65e2f772f02b0780638ea4f721bfe9b467
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-07 20:28:36 -0400

    add sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Message } from '@storage/message.emtity';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersController } from './services/users/users.controller';
 import { ReportsController } from './services/reports/reports.controller';
 
 @Module({
-  imports: [MessagesModule, UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    MessagesModule,
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController, UsersController, ReportsController],
   providers: [AppService],
 })
