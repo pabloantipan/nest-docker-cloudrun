87775d1a273185c905bd01a82e28717654d3a2c7
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-09 18:04:34 -0400

    add repository for creating new users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './models/dtos/create-user.dto';
+import { UsersService } from '@services/users/users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('/auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+    this.usersService.create(body.email, body.password);
+  }
+}
