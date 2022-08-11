db5105be7a1632e180209ae01c240d10ef235f18
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-10 23:01:11 -0400

    add basic hook in entity

--- src/users/models/user.entity.ts
+++ src/users/models/user.entity.ts
@@ -1,11 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Inserted User with id', this.id);
+  }
 }
