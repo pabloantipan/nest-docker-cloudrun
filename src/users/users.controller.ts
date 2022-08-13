be9e9806ca3c3b5ff730cf098fcd8b3a7de3594f
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-12 20:00:05 -0400

    add get user by id, get all users by criteria, delete by id, patch by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './models/dtos/create-user.dto';
 import { UsersService } from '@services/users/users.service';
+import { UpdateUserDto } from './models/dtos/update-user.dto';
 
 @Controller('/auth')
 export class UsersController {
@@ -10,5 +20,25 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUseR(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
