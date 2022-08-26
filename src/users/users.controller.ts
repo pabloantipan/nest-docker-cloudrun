847a7142979e0f0544f60874e38948cf23dbfc87
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-26 15:57:13 -0400

    add currect user decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UpdateUserDto } from './models/dtos/update-user.dto';
 import { Serialize } from '@interceptors/serialize.interceptor';
 import { UserDto } from './models/dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from '@decorators/users.decorator';
 
 @Controller('/auth')
 @Serialize(UserDto)
@@ -24,6 +25,21 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
+
+  // @Get('whoami')
+  // async whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
+  @Get('whoami')
+  async whoAmI(@CurrentUser() user: string) {
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
