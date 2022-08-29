b90fd30780e7585a1c5f5847cf951d7fbb4364cc
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-28 23:23:21 -0400

    add auth service base testing

--- src/services/auth/auth.service.spec.ts
+++ src/services/auth/auth.service.spec.ts
@@ -1,10 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@guards/auth.guard';
+import { UsersService } from '@services/users/users.service';
 
 it('can create an instance of auth service', async () => {
+  // Mocking users service
+  const mockUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
   const module = await Test.createTestingModule({
-    providers: [AuthService],
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: mockUsersService,
+      },
+    ],
   }).compile();
 
   const service = module.get(AuthService);
