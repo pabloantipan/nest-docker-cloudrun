90bc67a9d143583fe6e38201d0ce426edef3041b
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-28 23:40:18 -0400

    fix auth service testing

--- src/services/auth/auth.service.spec.ts
+++ src/services/auth/auth.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@guards/auth.guard';
 import { UsersService } from '@services/users/users.service';
+import { User } from '@users/models/user.entity';
 
 it('can create an instance of auth service', async () => {
   // Mocking users service
-  const mockUsersService = {
+  const mockUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
