b7694b475810c3414c7f5673b9be9a432b6100aa
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-08-31 00:10:12 -0400

    add some tests of users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '@services/auth/auth.service';
+import { UsersService } from '@services/users/users.service';
+import { User } from './models/user.entity';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let mockUsersService: Partial<UsersService>;
+  let mockAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    mockUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'sdfsd@sdf.cl',
+          password: '1234fg',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: '123123' } as User]);
+      },
+      remove: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'werwe@as.cl',
+          password: '123sad',
+        } as User);
+      },
+      update: (id: number, attrs: Partial<User>) => {
+        return Promise.resolve({ id, ...attrs } as User);
+      },
+    };
+    mockAuthService = {
+      signup: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +60,12 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const mockEmail = 'asdf@sadf.com';
+    const users = await controller.findAllUsers(mockEmail);
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual(mockEmail);
   });
 });
